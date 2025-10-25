9ad87638a8addf9e7e4cb32e67be477f2af59471
Swikriti Chourasia <swikriti.333@gmail.com>
2025-10-25 11:00:16 +0200

    Added definitions

--- src/test/java/stepDefinitions/steps.java
+++ src/test/java/stepDefinitions/steps.java
@@ -7,23 +7,29 @@ import io.cucumber.java.en.When;
 public class steps {
 
 
-    @Given("I am an authencticated github user")
-    public void i_am_an_authencticated_github_user() {
+    @Given("I am an authenticated github user")
+    public void i_am_an_authenticated_github_user() {
+        System.out.println("i_am_an_authenticated_github_user");
 
     }
     @When("I send a request to create a repository with name as {string} and with description {string}")
     public void i_send_a_request_to_create_a_repository_with_name_as_and_with_description(String string, String string2) {
-
+        System.out.println("i_send_a_request_to_create_a_repository_with_name_as_and_with_description");
     }
     @Then("the response code should be {int}")
     public void the_response_code_should_be(Integer int1) {
+        System.out.println("the_response_code_should_be");
 
     }
     @Then("the response should contain the repository name {string}")
     public void the_response_should_contain_the_repository_name(String string) {
+        System.out.println("the_response_should_contain_the_repository_name");
+
     }
     @Then("the response time should be less than {int} milliseconds")
     public void the_response_time_should_be_less_than_milliseconds(Integer int1) {
+        System.out.println("the_response_time_should_be_less_than_milliseconds");
+
 
     }
 
--- src/test/resources/createRepository.feature
+++ src/test/resources/createRepository.feature
@@ -3,7 +3,7 @@ Feature: Create git repository
   I want to create a new git repository
 
   Scenario: Create a valid git repository
-    Given I am an authencticated github user
+    Given I am an authenticated github user
     When I send a request to create a repository with name as "Selenium-gitrepo" and with description "git"
     Then the response code should be 200
     And the response should contain the repository name "Selenium-gitrepo"
